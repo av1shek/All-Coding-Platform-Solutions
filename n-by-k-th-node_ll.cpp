f186fb42202cff4745929b69fe9803198998b884
Rohan Sonawane <92113971+Rohans-7@users.noreply.github.com>
2022-10-13 23:39:41 +0530

    Find the N by K the element of the linked list
    This is a cpp solution to the problem find the N by K the element of the linked list.

---
+++ n-by-k-th-node_ll.cpp
@@ -0,0 +1,71 @@
+// C++ program to find N/K in a linked list solution
+#include <bits/stdc++.h>
+
+/* Structure to create a node of a linked list */
+struct Node {
+	int data;
+	Node* next;
+};
+
+/* Function to create a node of a linked list with given data */
+Node* newNode(int data)
+{
+	Node* new_node = new Node;
+	new_node->data = data;
+	new_node->next = NULL;
+	return new_node;
+}
+
+/* Function to find N/K node in the linked list */
+Node* fractionalNodes(Node* head, int k)
+{
+	if (k <= 0 || head == NULL)
+		return NULL;
+
+	Node* fractionalNode = NULL;
+	
+	int i = 0;
+	for (Node* temp = head; temp != NULL; temp = temp->next) {
+
+		// For every k nodes, we move N/Kth node
+		if (i % k == 0) {
+
+			// First time we see a multiple of k
+			if (fractionalNode == NULL)
+				fractionalNode = head;
+
+			else
+				fractionalNode = fractionalNode->next;
+		}
+		i++;
+	}
+	return fractionalNode;
+}
+
+// A utility function to print a linked list
+void printList(Node* node)
+{
+	while (node != NULL) {
+		cout<<node->data;
+		node = node->next;
+	}
+	cout<<endl;
+}
+int main(void)
+{
+	Node* head = newNode(1);
+	head->next = newNode(2);
+	head->next->next = newNode(3);
+	head->next->next->next = newNode(4);
+	head->next->next->next->next = newNode(5);
+	int k = 2;
+
+	cout<<"List is : ";
+	printList(head);
+
+	Node* answer = fractionalNodes(head, k);
+	cout<<"Fractional node is : ";
+	cout<<answer->data;
+
+	return 0;
+}
