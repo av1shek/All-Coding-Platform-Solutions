b88f44ddf9084329ed864c1476f4a96c7a8d97aa
SHEKHAR0008 <shekharhimanshu0008@gmail.com>
2022-10-13 11:11:04 +0530

    Solution of a problem of cses problem set.

---
+++ Codeforces/weirdalgo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+   long long int n;
+    cin>>n;
+    while(n!=1){
+          cout<<n<<" ";
+          if(n&1)
+              n = n*3 + 1;
+            else 
+               n/=2;
+    }
+    cout<<n<<" ";
+}
