4121e3d535c8d7dbb8867c89dfc4066103ecab07
mr-dubeyshivam7714 <91728038+mr-dubeyshivam7714@users.noreply.github.com>
2022-10-29 20:14:21 +0530

    Updated InsertionSort.cpp
    Updated InsertionSort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,31 +1,37 @@
-include<iostream>
-#include<vector>
+// Insertion sort in C++
+
+#include <iostream>
 using namespace std;
-void insertionSort(vector<int>&arr){
-  for(int i=1;i<arr.size(); i++){
-    int key=arr[i];
-    int j=i-1;
-    while(j>=0&&arr[j]>key){
-      arr[j+1]=arr[j];
-      j--;
-      arr[j+1]=key;
-    }
 
-
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
 }
-  for(int ele:arr){
-     cout<<ele<<" ";
 
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
 
-     }
-     cout<<"\n";
- 
- }
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
 
-
-
-int main(){
-  vector<int>arr={1,4,6,8,5,9};
-  insertionSort(arr);
-
+// Driver code
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  cout << "Sorted array in ascending order:\n";
+  printArray(data, size);
 }
