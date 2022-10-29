d247305bc9918890d348c7e830c23836204592d6
ankitkamdi <106524124+ankitkamdi@users.noreply.github.com>
2022-10-29 11:00:54 +0530

    Create subtwonum.cpp

---
+++ subtwonum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int f1, s2, sub;
+  cout << "Enter two numbers: ";
+  cin >> f1 >> s2;
+  sub = f1 - s2;
+  cout << f1 << " - " <<  s2 << " = " << sub;
+  return 0;
+}
